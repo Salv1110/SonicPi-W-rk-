9c103e027298d8cb95cf3048877babaa7d81a6b6
Salv1110 <salvador_rojas@student.davincischools.org>
2026-03-18 10:04:43 -0700

    Challenge-F.rb

--- Challenge-F.rb
+++ Challenge-F.rb
@@ -1,1 +1,13 @@
+# Welcome to Sonic Pi
 
+use_synth :piano
+#CHALLENGE F: Piano Glissando starter code
+
+use_synth :piano
+num = 21 # bc we start counting at 21
+
+87.times do
+  play(num)
+  sleep 0.25
+  num = num + 1
+end
